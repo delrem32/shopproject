68634ac40c1e4c2386d7caaa876ecdf42f882124
delrem32 <delrem32@gmail.com>
2020-06-01 17:51:37 +0200

    +order module init

--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { OrderRoutingModule } from './order-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
 import {NzIconModule, NzListModule} from 'ng-zorro-antd';
+import { OrderActiveComponent } from './order-active/order-active.component';
+import { OrderCompletedComponent } from './order-completed/order-completed.component';
+import { OrderFormComponent } from './order-form/order-form.component';
 
 
 @NgModule({
-  declarations: [OrderListComponent],
+  declarations: [OrderListComponent, OrderActiveComponent, OrderCompletedComponent, OrderFormComponent],
   imports: [
     CommonModule,
     OrderRoutingModule,
